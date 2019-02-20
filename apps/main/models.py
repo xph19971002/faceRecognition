608841be67ca76ec5d0959ede4a82c9b909c739c
XeeP-h <1342696614@qq.com>
2019-02-20 15:58:21 +0800

    模型层上传

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    t_id = models.AutoField(primary_key=True, default=1)
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    is_delete = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "test"
