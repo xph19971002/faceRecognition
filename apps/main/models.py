628a243a422e3bb337c2b571efd73eab265b78cb
XeeP-h <1342696614@qq.com>
2019-02-22 09:36:48 +0800

    模型测试

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Test(models.Model):
     t_id = models.AutoField(primary_key=True, default=1)
-    username = models.CharField(max_length=64)
-    password = models.CharField(max_length=64)
-    is_delete = models.IntegerField(default=0)
+    username = models.CharField(max_length=64, null=False)
+    password = models.CharField(max_length=64, null=False)
+    is_delete = models.BooleanField(default=False, null=False)
+    price = models.DecimalField(max_digits=7,decimal_places=2)
 
     class Meta:
         db_table = "test"
