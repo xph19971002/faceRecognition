c4b4b0135e6116a279a28b44e00ef22997279d3a
XeeP-h <1342696614@qq.com>
2019-05-19 17:15:28 +0800

    增加路由

--- faceRecognition/urls.py
+++ faceRecognition/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
-from django.contrib import admin
 import xadmin
+from django.conf.urls import url
+
+from apps.error import views as v3
 # from django.urls import path
 from apps.main import views as v1
 from apps.result import views as v2
@@ -10,5 +11,6 @@ urlpatterns = [
     url('admin/', xadmin.site.urls),
     url('^$', v1.index, name="index"),
     url('result/', v2.result, name='result'),
-    url('add/', v2.add, name="add")
+    url('add/', v2.add, name="add"),
+    url("404/", v3.error, name="error")
 ]
